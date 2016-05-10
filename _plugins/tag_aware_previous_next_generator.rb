0fcd9acb90b8466a17ca47ffc2fa953a62dba5e4
kobapan <kobapan@gmail.com>
2016-05-10 20:36:38 +0900

    publish

--- _plugins/tag_aware_previous_next_generator.rb
+++ _plugins/tag_aware_previous_next_generator.rb
@@ -1,3 +1,9 @@
+#how to use in template
+#
+#{{ page.tag }}'s post'
+#<a href="{{site.url}}{{ page.tag_previous.url }}">&laquo; {{ page.tag_previous.title }}</a>
+#<a href="{{site.url}}{{ page.tag_next.url }}">{{ page.tag_next.title }} &raquo;</a>
+#
 module Jekyll
   class TagAwarePreviousNextGenerator < Generator
 
@@ -17,7 +23,7 @@ module Jekyll
             tag_previous = nil
           end
 
-          if position && position > 1
+          if position && position > 0
             tag_next = posts[position - 1]
           else
             tag_next = nil
