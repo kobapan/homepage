19ba9001fda0de7759dd8ae4d9a191892721014c
kobapan <kobapan@gmail.com>
2019-01-30 23:03:11 +0900

    publish

--- _plugins/tocGenerator.rb
+++ _plugins/tocGenerator.rb
@@ -13,7 +13,7 @@ module Jekyll
     def toc_generate(html)
       # No Toc can be specified on every single page
       # For example the index page has no table of contents
-      return html if (! @context.environments.first["page"]["toc"] || @context.environments.first["page"]["toc"] == false)
+      return html if (! @context.environments.first["page"]["toc"])
 
       config = @context.registers[:site].config
 
